5f6eaacd260aa95f7ca5ba4f3ab8ed057a2d5b90
jehyunJung <toojey7@gmail.com>
2023-02-11 18:26:59 +0900

    fix: deleted spring security
    will be attached later

--- src/main/java/com/lazyworking/sagupalgu/user/exception/AccessTokenExpiredException.java
+++
@@ -1,8 +0,0 @@
-package com.lazyworking.sagupalgu.user.exception;
-
-public class AccessTokenExpiredException extends RuntimeException{
-    private String refreshToken;
-    public AccessTokenExpiredException(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
-}
