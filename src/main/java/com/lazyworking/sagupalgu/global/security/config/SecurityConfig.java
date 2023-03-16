b8d5a236ce5c8937164eb9b7fd65a1c85552a9e1
jehyunJung <toojey7@gmail.com>
2023-03-16 13:09:07 +0900

    feat: add authorization components
    CustomAuthorizationManager
    CustomAuthorizationFilter

--- src/main/java/com/lazyworking/sagupalgu/global/security/config/SecurityConfig.java
+++ src/main/java/com/lazyworking/sagupalgu/global/security/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.lazyworking.sagupalgu.global.security.config;
 
+import com.lazyworking.sagupalgu.global.security.filter.CustomAuthorizationFilter;
 import com.lazyworking.sagupalgu.global.security.handler.FormAccessDeniedHandler;
 import com.lazyworking.sagupalgu.global.security.handler.FormAuthenticationFailureHandler;
 import com.lazyworking.sagupalgu.global.security.handler.FormAuthenticationSuccessHandler;
+import com.lazyworking.sagupalgu.global.security.manager.CustomAuthorizationManager;
 import com.lazyworking.sagupalgu.global.security.provider.FormAuthenticationProvider;
 import com.lazyworking.sagupalgu.global.security.webdetails.FormAuthenticationDetailsSource;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -17,6 +20,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
@@ -24,21 +28,46 @@ import org.springframework.security.web.authentication.LoginUrlAuthenticationEnt
 @RequiredArgsConstructor
 @Order(1)
 public class SecurityConfig {
+    //인증 성공 핸들러
     private final FormAuthenticationSuccessHandler formAuthenticationSuccessHandler;
+    //인증 실패 핸들러
     private final FormAuthenticationFailureHandler formAuthenticationFailureHandler;
-
+    //인간 실패 핸들러
     private final FormAccessDeniedHandler formAccessDeniedHandler;
-
+    //로그인시 추가되는 사용자 관련 정보
     private final FormAuthenticationDetailsSource formAuthenticationDetailsSource;
-
+    //인증을 수행하는 authentication provider
     private final FormAuthenticationProvider formAuthenticationProvider;
 
+    //계층관계 정보를 가지는 Spring 내부 객체
+    @Bean
+    public RoleHierarchyImpl roleHierarchyImpl(){
+        return new RoleHierarchyImpl();
+    }
+
+    //authentication provider을 호출하는 authentication manager
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         ProviderManager providerManager = (ProviderManager) authenticationConfiguration.getAuthenticationManager();
         providerManager.getProviders().add(formAuthenticationProvider);
         return providerManager;
     }
+
+    //인가 검증을 수행하는 CustomAuthorizationManager
+    public CustomAuthorizationManager customAuthorizationManager() {
+        CustomAuthorizationManager customAuthorizationManager = new CustomAuthorizationManager();
+        //모든 접근을 허용하고자할 url 지정
+        customAuthorizationManager.setPermitAlls("/","/home","/denied**","/error**","/signIn","/login**");
+        //나머지 url 자원에 대해서는 인증을 필수적으로 진행하게끔 설정
+        customAuthorizationManager.setAuthenticated(true);
+        return customAuthorizationManager;
+    }
+
+    //authorization manager을 호출하는 authorization filter
+    public CustomAuthorizationFilter customAuthorizationFilter(){
+        return new CustomAuthorizationFilter(customAuthorizationManager());
+    }
+
 
     //정적 자원에 대한 보안 필터링 무시
     @Bean
@@ -48,11 +77,14 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        //기존의 보안 필터는 모두 통과하도록 설정
         httpSecurity
                 .authorizeHttpRequests()
-                .requestMatchers("/","/home","/denied**","/error**","/signIn","/usedItems","/login**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().permitAll();
+        //모든 인가 검증은 customauthorizationfilter에서 처리된다.
+        httpSecurity.addFilterAfter( customAuthorizationFilter(), AuthorizationFilter.class);
 
+        //로그인 관련 설정
         httpSecurity.formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/login_proc")
@@ -61,13 +93,15 @@ public class SecurityConfig {
                 .failureHandler(formAuthenticationFailureHandler)
                 .permitAll();
 
+        //spring security에서 발생하는 예외 처리 관련 설정
         formAccessDeniedHandler.setErrorPage("/denied");
-
         httpSecurity
                 .exceptionHandling()
                 .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
                 .accessDeniedPage("/denied")
                 .accessDeniedHandler(formAccessDeniedHandler);
+
         return httpSecurity.build();
+
     }
 }
---
+++ src/main/java/com/lazyworking/sagupalgu/global/security/manager/CustomAuthorizationManager.java
@@ -0,0 +1,86 @@
+package com.lazyworking.sagupalgu.global.security.manager;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.core.log.LogMessage;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcherEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CustomAuthorizationManager implements AuthorizationManager<HttpServletRequest> {
+    private static final AuthorizationDecision DENY = new AuthorizationDecision(false);
+
+    private List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> defaultMappings = new ArrayList<>();
+
+    private List<RequestMatcher> permitAlls = new ArrayList<>();
+
+    private List<RequestMatcher> deniedIps=new ArrayList<>();
+
+    private Boolean authenticated;
+    private final Log logger = LogFactory.getLog(getClass());
+
+    public void setPermitAlls(String... urls) {
+        for(String url: urls)
+            this.permitAlls.add(new AntPathRequestMatcher(url));
+    }
+
+    public void setDeniedIps(List<RequestMatcher> deniedIps) {
+        this.deniedIps = deniedIps;
+    }
+
+    public void setAuthenticated(Boolean authenticated) {
+        if(authenticated)
+            this.defaultMappings.add(new RequestMatcherEntry<>(new AntPathRequestMatcher("**"), AuthenticatedAuthorizationManager.authenticated()));
+    }
+
+    @Override
+    public AuthorizationDecision check(Supplier<Authentication> authentication, HttpServletRequest request) {
+        if (this.logger.isTraceEnabled())
+            this.logger.trace(LogMessage.format("Authorizing %s", request));
+
+
+        for (RequestMatcher ipMatcher : deniedIps) {
+            if(ipMatcher.matches(request))
+                return DENY;
+        }
+
+        for(RequestMatcher permitMatcher: permitAlls)
+            if(permitMatcher.matches(request))
+                return new AuthorizationDecision(true);
+
+        List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings = new ArrayList<>();
+        mappings.addAll(defaultMappings);
+
+        for (RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> mapping : mappings) {
+            RequestMatcher matcher = mapping.getRequestMatcher();
+            RequestMatcher.MatchResult matchResult = matcher.matcher(request);
+            if (matchResult.isMatch()) {
+                AuthorizationManager<RequestAuthorizationContext> manager = mapping.getEntry();
+                if (this.logger.isTraceEnabled()) {
+                    this.logger.trace(LogMessage.format("Checking authorization on %s using %s", request, manager));
+                }
+                return manager.check(authentication,
+                        new RequestAuthorizationContext(request, matchResult.getVariables()));
+            }
+        }
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(LogMessage.of(() -> "Denying request since did not find matching RequestMatcher"));
+        }
+        return DENY;
+    }
+}
